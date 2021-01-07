b7f473757f83a06a118306b8d557dab15fa27268
jung <myj@srokinc.co.kr>
2021-01-07 15:27:20 +0900

    Simplify main()

--- sha256.c
+++ sha256.c
@@ -314,35 +314,58 @@ int FILE_SHA256_Encrpyt(char* path,  uint8_t *pszDigest)
 
 	return SHA256_DIGEST_VALUELEN;
 }
-
+#define VERSION 1
 // ref : https://github.com/ilvn/SHA256/tree/master/mark2 
 int main(void)
 {
-	// char *buf[] = {
-	// 	"abc"
-	// };
 	uint8_t hash[SHA256_DIGEST_VALUELEN];
 	size_t i, j;
 
-	char *path = "/home/moon/workspace/kisa_sha256/test_readme.md"; // file path
-	int Encrpyt_result = FILE_SHA256_Encrpyt(path,hash);
-	// printf("%d",Encrpyt_result);
-	if (Encrpyt_result)
-	{
-		printf("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n"); 
-		for (j = 0; j < SHA256_DIGEST_VALUELEN; j++)
-			printf("%02x%s", hash[j], ((j % 4) == 3) ? "" : "");
-		printf("\n++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n"); 
-	} else {
-		printf("\nerrorn"); 
-	}
-	// for (i = 0; i < (sizeof(buf) / sizeof(buf[0])); i += 2) {
-	// 	SHA256_Encrpyt(buf[i], strlen(buf[i]), hash);
-	// 	printf("input = '%s'\ndigest: %s\nresult: ", buf[i], buf[i + 1]);
-	// 	for (j = 0; j < SHA256_DIGEST_VALUELEN; j++)
-	// 		printf("%02x%s", hash[j], ((j % 4) == 3) ? " " : "");
-	// 	printf("\n\n");
-	// }
+	#if (VERSION == 1) /* string buffer SHA256*/
+		char *buf[] = {
+			"",
+			"e3b0c442 98fc1c14 9afbf4c8 996fb924 27ae41e4 649b934c a495991b 7852b855",
+
+			"abc",
+			"ba7816bf 8f01cfea 414140de 5dae2223 b00361a3 96177a9c b410ff61 f20015ad",
+
+			"abcdbcdecdefdefgefghfghighijhijkijkljklmklmnlmnomnopnopq",
+			"248d6a61 d20638b8 e5c02693 0c3e6039 a33ce459 64ff2167 f6ecedd4 19db06c1",
+
+			"The quick brown fox jumps over the lazy dog",
+			"d7a8fbb3 07d78094 69ca9abc b0082e4f 8d5651e4 6d3cdb76 2d02d0bf 37c9e592",
+
+			"The quick brown fox jumps over the lazy cog", /* avalanche effect test */
+			"e4c4d8f3 bf76b692 de791a17 3e053211 50f7a345 b46484fe 427f6acc 7ecc81be",
+
+			"bhn5bjmoniertqea40wro2upyflkydsibsk8ylkmgbvwi420t44cq034eou1szc1k0mk46oeb7ktzmlxqkbte2sy",
+			"9085df2f 02e0cc45 5928d0f5 1b27b4bf 1d9cd260 a66ed1fd a11b0a3f f5756d99"
+		};
+		for (i = 0; i < (sizeof(buf) / sizeof(buf[0])); i += 2) {
+			int Encrpyt_result = SHA256_Encrpyt(buf[i], strlen(buf[i]), hash);
+			if (Encrpyt_result)
+			{
+				printf("input = '%s'\ndigest: %s\nresult: ", buf[i], buf[i + 1]);
+				for (j = 0; j < SHA256_DIGEST_VALUELEN; j++)
+					printf("%02x%s", hash[j], ((j % 4) == 3) ? " " : "");
+				printf("\n\n");
+			} else {
+				printf("\nerrorn");
+			}
+		}
+	#elif (VERSION == 2) /* File SHA256*/
+		char *path = "/home/moon/workspace/kisa_sha256/test_readme.md"; // file path
+		int Encrpyt_result = FILE_SHA256_Encrpyt(path,hash);
+		if (Encrpyt_result)
+		{
+			printf("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n"); 
+			for (j = 0; j < SHA256_DIGEST_VALUELEN; j++)
+				printf("%02x%s", hash[j], ((j % 4) == 3) ? "" : "");
+			printf("\n++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n"); 
+		} else {
+			printf("\nerrorn"); 
+		}
+	#endif
 
 	return 0;
 }
